f564a2b06cecb49bb8508124a97246d5a517451b
JiuZhiJiao <jiuzhijiao@gmail.com>
2021-03-24 01:22:53 +1100

    Create Day02

---
+++ Day002/MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+beatles[1]
+
+
+let colors = Set(["red", "green", "blue"])
+let colors2 = Set(["red", "green", "blue", "red", "blue"])
